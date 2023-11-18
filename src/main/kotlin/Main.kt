b7915ee2f6e7de5363f72d4f7af8227208314c85
eoghan <20101374@mail.wit.ie>
2023-11-18 23:07:13 +0000

    Created starter menus in the Main.kt and initilised driver XML storage along with some main menu functions in main. Imported ScannerInput to read input from the users for the menus

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,89 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import controllers.DriverAPI
+import persistence.XMLSerializer
+import utitlities.ScannerInput.readNextInt
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class CarManageApp {
+    private val driverApi = DriverAPI.DriverAPI(XMLSerializer(File("drivers.xml")))
+
+    fun main() {
+        runMainMenu()
+    }
+
+    fun runMainMenu() {
+
+    }
+
+    fun mainMenu(): Int {
+        return readNextInt(
+            """
+            ╔════════════════════════════════╗
+            ║        TAG Community Car       ║
+            ╚════════════════════════════════╝
+            ╔════════════════════════════════╗
+            ║        Main Menu               ║
+            ║                                ║
+            ║   1.) Driver Menu              ║
+            ║   2.) Trip Menu                ║
+            ║   3.) Admin Menu               ║
+            ║   0.) Exit                     ║
+            ╚════════════════════════════════╝
+            ==>>""".trimMargin(">")
+        )
+    }
+
+    fun DriverMenu(): Int {
+        return readNextInt(
+            """
+            ╔════════════════════════════════╗
+            ║        TAG Community Car       ║
+            ╚════════════════════════════════╝
+            ╔════════════════════════════════╗
+            ║        Main Menu               ║
+            ║    1.) Check in                ║ 
+            ║                                ║
+            ║    2.) View Schedule           ║              
+            ║                                ║
+            ║    3.) View previous Trips     ║
+            ║                                ║
+            ║   0.) Exit                     ║
+            ╚════════════════════════════════╝
+            ==>>""".trimMargin(">")
+        )
+    }
+
+    fun TripMenu(): Int {
+        return readNextInt(
+            """
+            ╔════════════════════════════════╗
+            ║        TAG Community Car       ║
+            ╚════════════════════════════════╝
+            ╔════════════════════════════════╗
+            ║        Trip Menu               ║
+            ║    1.) Start A trip            ║ 
+            ║                                ║
+            ║    2.) View future Trips       ║              
+            ║                                ║
+            ║    3.) View previous Trips     ║
+            ║                                ║
+            ║   0.) Exit                     ║
+            ╚════════════════════════════════╝
+            ==>>""".trimMargin(">")
+        )
+    }
+
+    fun runDriverMenu() {
+        // Implement driver menu logic
+        println("Driver Menu")
+    }
+
+    fun runTripMenu() {
+        // Implement trip menu logic
+        println("Trip Menu")
+    }
+
+    fun runAdminMenu() {
+        // Implement admin menu logic
+        println("Admin Menu")
+    }
 }
--- src/main/kotlin/controllers/DriverAPI.kt
+++ src/main/kotlin/controllers/DriverAPI.kt
@@ -4,3 +4,8 @@ import models.Driver
 import persistence.Serializer
 import java.util.*
 import kotlin.collections.ArrayList
+class DriverAPI {
+    class DriverAPI(serializerType: Serializer) {
+        private var drivers = ArrayList<Driver>()
+        private var serializer: Serializer = serializerType
+    }}
